a8b73906918f00647a85e4b6f18b05e5599cb507
Korhan Mutludoğan <korhanmd@gmail.com>
2021-01-28 13:05:48 +0300

    Initial commit

---
+++ occupancy_grid.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+// Sensor characteristic: Min and Max ranges of the beams
+double Zmax = 5000, Zmin = 170;
+// Defining free cells(lfree), occupied cells(locc), unknown cells(l0) log odds values
+double l0 = 0, locc = 0.4, lfree = -0.4;
+// Grid dimensions
+double gridWidth = 100, gridHeight = 100;
+// Map dimensions
+double mapWidth = 30000, mapHeight = 15000;
+// Robot size with respect to the map 
+double robotXOffset = mapWidth / 5, robotYOffset = mapHeight / 3;
+// Defining an l vector to store the log odds values of each cell
+vector< vector<double> > l(mapWidth/gridWidth, vector<double>(mapHeight/gridHeight));
+
+double inverseSensorModel(double x, double y, double theta, double xi, double yi, double sensorData[])
+{
+    return 0.4;
+}
+
+void occupancyGridMapping(double Robotx, double Roboty, double Robottheta, double sensorData[])
+{
+    
+}
+
+int main()
+{
+    double timeStamp;
+    double measurementData[8];
+    double robotX, robotY, robotTheta;
+
+    FILE* posesFile = fopen("poses.txt", "r");
+    FILE* measurementFile = fopen("measurement.txt", "r");
+
+    // Scanning the files and retrieving measurement and poses at each timestamp
+    while (fscanf(posesFile, "%lf %lf %lf %lf", &timeStamp, &robotX, &robotY, &robotTheta) != EOF) {
+        fscanf(measurementFile, "%lf", &timeStamp);
+        for (int i = 0; i < 8; i++) {
+            fscanf(measurementFile, "%lf", &measurementData[i]);
+        }
+        occupancyGridMapping(robotX, robotY, (robotTheta / 10) * (M_PI / 180), measurementData);
+    }
+    
+    // Displaying the map
+    for (int x = 0; x < mapWidth / gridWidth; x++) {
+        for (int y = 0; y < mapHeight / gridHeight; y++) {
+            cout << l[x][y] << " ";
+        }
+    }
+    
+    return 0;
+}
